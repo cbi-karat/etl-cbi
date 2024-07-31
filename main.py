4b989a1c8ca6a12e1e721ce28c885ee46c2eae20
KirillGrebn3v <grebn3v.k@yandex.ru>
2024-07-31 10:45:30 +0300

    ETL release

--- main.py
+++ main.py
@@ -9,16 +9,19 @@ import requests as req
 from credentials.KDL_passwords import KDL
 
 
-def request(dbname, start_date: type | None = None, end_date: type | None = None):  # noqa: C901, PLR0911, PLR0912
-    datesymbols = 10
-    error = 3
-    errors = ["Dates are required", "Dates aren't required", "Incorrect date", "The error isn't defined"]
+def request(dbname, start_date: type | None = None, end_date: type | None = None):
     correctinput, dtrequired = getjson_test(dbname)
     if not correctinput:
         return "The table wasn't found"
     if start_date is None and end_date is None:
         dates = False
-    elif (
+        return check2(dates, dtrequired, dbname, start_date, end_date)
+    return check1(dtrequired, dbname, start_date, end_date)
+
+
+def check1(dtrequired, dbname, start_date, end_date):
+    datesymbols = 10
+    if (
         start_date[0:2].isdigit()
         and start_date[3:5].isdigit()
         and start_date[6:].isdigit()
@@ -32,10 +35,10 @@ def request(dbname, start_date: type | None = None, end_date: type | None = None
         try:
             start_date = date(int(start_date[2]), int(start_date[1]), int(start_date[0]))
         except ValueError:
-            error = 2
-            return errors[error]
+            return "Incorrect date"
         else:
             end_date = start_date
+            return check2(dates, dtrequired, dbname, start_date, end_date)
     elif (
         start_date[0:2].isdigit()
         and start_date[3:5].isdigit()
@@ -56,24 +59,58 @@ def request(dbname, start_date: type | None = None, end_date: type | None = None
         try:
             start_date = date(int(start_date[2]), int(start_date[1]), int(start_date[0]))
         except ValueError:
-            error = 2
-            return errors[error]
+            return "Incorrect date"
         try:
             end_date = date(int(end_date[2]), int(end_date[1]), int(end_date[0]))
         except ValueError:
-            error = 2
-            return errors[error]
+            return "Incorrect date"
+        else:
+            return check2(dates, dtrequired, dbname, start_date, end_date)
     else:
-        return "Incorrect date information"
+        return "Incorrect date"
+
+
+def check2(dates, dtrequired, dbname, start_date, end_date):
+    errors = ["Dates are required", "Dates aren't required", "The error isn't defined"]
+    error = 2
     if not dates and dtrequired:
         error = 0
     elif dates and not dtrequired:
         error = 1
     elif dates and dtrequired:
-        return getjson(dbname, start_date, end_date)
+        if start_date > end_date:
+            return "Incorrect date"
+        return distribution(dbname, start_date, end_date)
     elif not dates and not dtrequired:
         return getjson_nodates(dbname)
     return errors[error]
+
+
+def distribution(dbname, start_date, end_date):
+    last_month = date(int(str(end_date)[0:4]), int(str(end_date)[5:7]), 1)
+    try:
+        second_month = date(int(str(start_date)[0:4]), int(str(start_date)[5:7]) + 1, 1)
+    except ValueError:
+        second_month = date(int(str(start_date)[0:4]) + 1, 1, 1)
+    if second_month > last_month:
+        periods_list = [start_date, end_date]
+    elif second_month == last_month and last_month != end_date:
+        periods_list = [start_date, second_month, end_date]
+    elif second_month == last_month and last_month == end_date:
+        periods_list = [start_date, end_date]
+    else:
+        periods_list = [start_date]
+        interim_date = second_month
+        while interim_date != last_month:
+            periods_list.append(interim_date)
+            try:
+                interim_date = date(int(str(interim_date)[0:4]), int(str(interim_date)[5:7]) + 1, 1)
+            except ValueError:
+                interim_date = date(int(str(interim_date)[0:4]) + 1, 1, 1)
+        periods_list.append(last_month)
+        if last_month != end_date:
+            periods_list.append(end_date)
+    return getjson(dbname, periods_list)
 
 
 def getjson_test(dbname):
@@ -81,10 +118,12 @@ def getjson_test(dbname):
     query = json.dumps(query)
     login = KDL["login"]
     login = login.encode("utf-8")
+    password = KDL["password"]
+    url = KDL["url"]
     try:
         response = req.post(
-            KDL["url"],
-            auth=(login, KDL["password"]),
+            url,
+            auth=(login, password),
             json=query,
             timeout=100,
         )
@@ -94,8 +133,8 @@ def getjson_test(dbname):
             query = {"DBName": dbname}
             query = json.dumps(query)
             response = req.post(
-                KDL["url"],
-                auth=(login, KDL["password"]),
+                url,
+                auth=(login, password),
                 json=query,
                 timeout=100,
             )
@@ -108,18 +147,29 @@ def getjson_test(dbname):
         return True, True
 
 
-def getjson(dbname, start_date, end_date):
-    query = {"DBName": dbname, "НачалоПериода": start_date.isoformat(), "КонецПериода": end_date.isoformat()}
-    query = json.dumps(query)
+def getjson(dbname, periods_list):
+    responses = []
     login = KDL["login"]
     login = login.encode("utf-8")
-    response = req.post(
-        KDL["url"],
-        auth=(login, KDL["password"]),
-        json=query,
-        timeout=100,
-    )
-    return convert_to_dataframe(response)
+    password = KDL["password"]
+    url = KDL["url"]
+    with req.Session() as session:
+        for i in range(len(periods_list) - 1):
+            start_date = periods_list[i]
+            end_date = periods_list[i + 1]
+            query = {"DBName": dbname, "НачалоПериода": start_date.isoformat(), "КонецПериода": end_date.isoformat()}
+            query = json.dumps(query)
+            response = session.post(
+                url,
+                auth=(login, password),
+                json=query,
+                timeout=100,
+            )
+            response = response.json()
+            response = json.loads(response["DBData"])
+            response = response["Результат"]
+            responses += response
+    return convert_to_dataframe(responses)
 
 
 def getjson_nodates(dbname):
@@ -127,17 +177,22 @@ def getjson_nodates(dbname):
     query = json.dumps(query)
     login = KDL["login"]
     login = login.encode("utf-8")
+    password = KDL["password"]
+    url = KDL["url"]
     response = req.post(
-        KDL["url"],
-        auth=(login, KDL["password"]),
+        url,
+        auth=(login, password),
         json=query,
         timeout=100,
     )
+    response = response.json()
+    response = json.loads(response["DBData"])
+    response = response["Результат"]
     return convert_to_dataframe(response)
 
 
 def convert_to_dataframe(response):
-    response = response.json()
-    response = json.loads(response["DBData"])
-    response = response["Результат"]
+    if response == []:
+        return """There is no data for this period, or this table uses grouping by month.
+If the table uses month grouping, try using the format 01.MM.YYYY"""
     return pd.DataFrame(response)
