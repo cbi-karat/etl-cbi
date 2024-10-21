82288fbb6f042b74c61b5099b963db85bdc8c73d
KirillGrebn3v <grebn3v.k@yandex.ru>
2024-10-21 22:30:39 +0300

    Tests improvements

--- src/tests/test_get_karat_db.py
+++ src/tests/test_get_karat_db.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from sys import path
 
-import pandas as pd
 import pytest
 
 path.append(str(Path(__file__).resolve().parents[1]))
@@ -10,45 +9,167 @@ import get_karat_db
 
 def test_missing_dates_error():
     with pytest.raises(get_karat_db.MissingDatesError):
-        _ = get_karat_db.get_table_from_kdl("Продажи_ПродажиФакт")
+        get_karat_db.get_table_from_kdl("Продажи_ПродажиФакт")
 
 
 def test_extra_dates_error():
     with pytest.raises(get_karat_db.ExtraDatesError):
-        _ = get_karat_db.get_table_from_kdl("Продажи_НСИ_SKU", "01.01.2000")
+        get_karat_db.get_table_from_kdl("Продажи_НСИ_SKU", "01.01.2000")
 
 
 def test_table_name_error():
     with pytest.raises(get_karat_db.InternalServerError):
-        _ = get_karat_db.get_table_from_kdl("Ъ")
+        get_karat_db.get_table_from_kdl("Ъ")
 
 
-def test_incorrect_date_error_1():
+def test_incorrect_date_error():
     with pytest.raises(get_karat_db.IncorrectDateError):
-        _ = get_karat_db.get_table_from_kdl("Продажи_ПродажиФакт", "01.32.2000")
+        get_karat_db.get_table_from_kdl("Продажи_ПродажиФакт", "01.32.2000")
 
-
-def test_incorrect_date_error_2():
     with pytest.raises(get_karat_db.IncorrectDateError):
-        _ = get_karat_db.get_table_from_kdl("Продажи_ПродажиФакт", "01.02.2000", "01.01.2000")
+        get_karat_db.get_table_from_kdl("Продажи_ПродажиФакт", "01.02.2000", "01.01.2000")
 
-
-def test_incorrect_date_error_3():
     with pytest.raises(get_karat_db.IncorrectDateError):
-        _ = get_karat_db.get_table_from_kdl("Продажи_ПродажиФакт", "Ъ")
+        get_karat_db.get_table_from_kdl("Продажи_ПродажиФакт", "Ъ")
 
 
 def test_empty_dataframe_error():
-    assert type(get_karat_db.get_table_from_kdl("Продажи_ПродажиФакт", "01.01.2000")) is pd.DataFrame  # noqa: S101
+    assert get_karat_db.get_table_from_kdl("Продажи_ПродажиФакт", "01.01.2000").empty  # noqa: S101
 
 
-def test_no_dates():
-    assert type(get_karat_db.get_table_from_kdl("Финансы_НСИ_SKU")) is pd.DataFrame  # noqa: S101
+def test_table_without_dates_getting():
+    assert list(get_karat_db.get_table_from_kdl("Финансы_НСИ_SKU").columns) == [  # noqa: S101
+        "КодПродукта1С",
+        "НаименованиеПродукта1С",
+        "Бренд",
+        "Суббренд",
+        "ТоварнаяКатегория",
+        "ТоварнаяПодкатегория",
+        "НомерФормулыРецепта",
+        "НаименованиеФормулыРецепта",
+        "ФасовочноеОборудование",
+        "ЛинияГрупповойУпаковки",
+        "ТехнологическийПроцессВарка",
+        "ТехнологическийПроцессИзмельчитель",
+        "ПроцессОхлаждения",
+        "Участок",
+        "ПроизводственнаяПлощадка",
+        "Производство",
+        "ФорматТипУпаковки",
+        "Группировка",
+        "Категория4",
+        "Категория3",
+        "Категория2",
+        "Категория1",
+        "Категория0",
+        "МастерКодСКЮ",
+        "МастерНаименованиеПродукта",
+        "УкрупненныйКодСКЮ",
+        "УкрупненноеНаименованиеСКЮ",
+        "Вкус",
+        "Сегмент",
+        "ТипУпаковки",
+        "Группировка4",
+        "Группировка5",
+        "Группировка6",
+        "АссортиментнаяГруппа",
+        "Скрывать",
+        "Вид_продукции",
+        "GTIN",
+        "Действующий",
+        "Вес",
+        "КоэффициентЕдиницыДляОтчетов",
+        "Вес1шт",
+        "Фасовка1местаШт",
+        "Вес1места",
+    ]
 
 
-def test_1_date():
-    assert type(get_karat_db.get_table_from_kdl("Продажи_ПродажиФакт", "01.08.2024")) is pd.DataFrame  # noqa: S101
+def test_table_with_dates_getting():
+    table = get_karat_db.get_table_from_kdl("Продажи_ПродажиФакт", "01.08.2024")
 
+    assert list(table.columns) == [  # noqa: S101
+        "Период",
+        "КлиентКод",
+        "Артикул",
+        "ПериодМесяц",
+        "ИсточникДанных",
+        "ИДЗаписи",
+        "ФО",
+        "ТипЦен",
+        "ТипДанных",
+        "Объем",
+        "ОбъемBaseLineКг",
+        "ОбъемПромоКг",
+        "Количество",
+        "КоличествоBaseLineШт",
+        "КоличествоПромоШт",
+        "ВыручкаПоБПЛБезНДС",
+        "ВыручкаПоПЛКБезНДС",
+        "ВыручкаNetБезНДС",
+        "ВыручкаNetBaseLineБезНДС",
+        "ВыручкаNetПромоБезНДС",
+        "ВыручкаNetсНДС",
+        "СкидкаБПЛПЛКбезНДС",
+        "СкидкаПЛКNetбезНДС",
+        "СуммаСкидкиФакт",
+        "ВыручкаПромоФакт",
+        "СуммаРБПромоФакт",
+        "СуммаПеременнойСебестоимостиПромоФакт",
+        "СуммаЛогистикиПромоФакт",
+        "СуммаРБФакт",
+        "СуммаПрочиеКУФакт",
+        "СуммаПеременнойСебестоимостиФакт",
+        "СуммаЛогистикиФакт",
+        "Склад",
+        "ВидЦены",
+        "Скидка_Наценка",
+    ]
 
-def test_2_dates():
-    assert type(get_karat_db.get_table_from_kdl("Продажи_ПродажиФакт", "01.08.2024", "02.08.2024")) is pd.DataFrame  # noqa: S101
+    assert table["Период"][table.first_valid_index()] == "2024-08-01T00:00:00"  # noqa: S101
+
+    assert table["Период"][table.last_valid_index()] == "2024-08-01T00:00:00"  # noqa: S101
+
+    table = get_karat_db.get_table_from_kdl("Продажи_ПродажиФакт", "01.08.2024", "02.08.2024")
+
+    assert list(table.columns) == [  # noqa: S101
+        "Период",
+        "КлиентКод",
+        "Артикул",
+        "ПериодМесяц",
+        "ИсточникДанных",
+        "ИДЗаписи",
+        "ФО",
+        "ТипЦен",
+        "ТипДанных",
+        "Объем",
+        "ОбъемBaseLineКг",
+        "ОбъемПромоКг",
+        "Количество",
+        "КоличествоBaseLineШт",
+        "КоличествоПромоШт",
+        "ВыручкаПоБПЛБезНДС",
+        "ВыручкаПоПЛКБезНДС",
+        "ВыручкаNetБезНДС",
+        "ВыручкаNetBaseLineБезНДС",
+        "ВыручкаNetПромоБезНДС",
+        "ВыручкаNetсНДС",
+        "СкидкаБПЛПЛКбезНДС",
+        "СкидкаПЛКNetбезНДС",
+        "СуммаСкидкиФакт",
+        "ВыручкаПромоФакт",
+        "СуммаРБПромоФакт",
+        "СуммаПеременнойСебестоимостиПромоФакт",
+        "СуммаЛогистикиПромоФакт",
+        "СуммаРБФакт",
+        "СуммаПрочиеКУФакт",
+        "СуммаПеременнойСебестоимостиФакт",
+        "СуммаЛогистикиФакт",
+        "Склад",
+        "ВидЦены",
+        "Скидка_Наценка",
+    ]
+
+    assert table["Период"][table.first_valid_index()] == "2024-08-01T00:00:00"  # noqa: S101
+
+    assert table["Период"][table.last_valid_index()] == "2024-08-02T00:00:00"  # noqa: S101
